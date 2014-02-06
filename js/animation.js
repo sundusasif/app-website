2de4aa4235ae95be6d88610a3df7aadc3d4bc294
Sundus Asif <sundus_06@hotmail.com>
2014-02-06 09:41:35 -0500

    Adding animation

---
+++ js/animation.js
@@ -0,0 +1,23 @@
+var $win = $(window);
+var $sun = $('.sun');
+var $dipper = $('.dipper');
+var $ships =$('.ships img');
+
+$win.on('scroll', function() {
+	//console.log($win.scrollTop());
+	$sun.css('transform', 'rotate(' + $win.scrollTop() / 5 + 'deg)');
+	});
+	
+
+$('.dipper-section').waypoint(function () {
+	
+	$dipper.addClass('js-dipper-animate');
+	
+	}, { offset: '60%' });
+	
+$('.icons-section').waypoint(function (){
+	$ships.eq(0).addClass('js-ships-delay-1');
+	$ships.eq(2).addClass('js-ships-delay-3');
+	$ships.eq(3).addClass('js-ships-delay-2');
+	$ships.addClass('js-ships-animate');
+}, { offset: '60%' });
